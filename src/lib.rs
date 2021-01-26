7533ca15d7b4f84dd820b3df2c1ad503fa6383a9
peter_todd <ptr.tdd@gmail.com>
2021-01-26 22:17:00 +0000

    small steps...

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 mod utils;
 
+// use std::io;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -14,6 +18,25 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, shp-contour-wasm!");
+pub fn greet(name: &str) {
+    alert(&format!("Hello {}, from shp-contour-wasm!", name));
+}
+#[wasm_bindgen]
+pub async fn fetch_shp(url: String) -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors); //probably shouldn't need CORS actually
+
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+    request.headers().set("Accept", "application/zip")?;
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    let _data = JsFuture::from(resp.array_buffer()?).await?;
+    
+
+    Ok(JsValue::from_str("TBD"))
 }
