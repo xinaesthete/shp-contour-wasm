e5fbb98031afb5c4af980d4f57895b5f117805b0
peter_todd <ptr.tdd@gmail.com>
2021-01-28 10:42:57 +0000

    add WorkerGlobalScope feature. some comments / rearrangement.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 mod utils;
-mod zip_util;
 
 use std::io;
 // use std::io::*; //woe betide
@@ -15,24 +14,17 @@ use shapefile::dbase::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     utils::set_panic_hook();
     Ok(())
 }
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello {}, from shp-contour-wasm!", name));
-}
 
-
-
+/// Retrieve a shapefile in zip format and return mesh data asynchronously.
+/// (assumes that input is similar to OS Terr 50 data:
+/// each zip having a *line.shp, *line.dbf, *point.shp, *point.dbf
+/// it'll probably fail somewhat ungraciously with data from any other source)
 #[wasm_bindgen]
 pub async fn fetch_shp(url: String) -> Result<MarshallGeometry, JsValue> {
     let mut opts = RequestInit::new();
@@ -42,7 +34,14 @@ pub async fn fetch_shp(url: String) -> Result<MarshallGeometry, JsValue> {
     let request = Request::new_with_str_and_init(&url, &opts)?;
     request.headers().set("Accept", "application/zip")?;
 
+    //I want a WorkerGlobalScope here in place of window.
+    //made relevant change to Cargo.toml, looks like I'll need something more elaborate to get
+    //WorkerGlobalScope
+    //I only want it to work in worker, but I suppose I could follow this approach
+    //https://github.com/rustwasm/gloo/blob/994d683f64fc04380f495fafb356521f346eff5f/crates/timers/src/callback.rs
+    //to make it still work in window & even also hypothetically extend to work in node.
     let window = web_sys::window().unwrap();
+    //let global = js_sys::global(); //TODO: add enum wrapper to utils.rs
     let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
     assert!(resp_value.is_instance_of::<Response>());
     let resp: Response = resp_value.dyn_into().unwrap();
@@ -50,15 +49,15 @@ pub async fn fetch_shp(url: String) -> Result<MarshallGeometry, JsValue> {
     let data = JsFuture::from(resp.array_buffer()?).await?;
     //get data into a form readable by other rust methods
     let d = js_sys::Uint8Array::new(&data);
-    // if we used &[u8] rather than vec, it'd already implement Read trait 
-    // (and may be more efficient, living on stack?)
     let v = io::Cursor::new(d.to_vec());
     let reader = io::BufReader::new(v);
     //compute results (should be similar to already implemented code)
     let (geo_3d, triangles) = shp_main(reader).expect("err");
-    //marshall results back into JsValues (preferably SharedArrayBuffers)
-    //perhaps a geometry struct that is also represented in TS
     
+    //marshall results back into JsValues (preferably SharedArrayBuffers)
+    
+    //this is unsafe because we make 'views' of our data in JS typed arrays
+    //(as yet, not SharedArrayBuffer, which could be even more unsafe?)
     unsafe {
         Ok(marshall_geometry_to_js(geo_3d, triangles))
     }
@@ -67,11 +66,13 @@ pub async fn fetch_shp(url: String) -> Result<MarshallGeometry, JsValue> {
 //wasm_bindgen types cannot have lifetime specifiers
 //also seem to be pretty limited in available types, lots of 'copy is not specified' complaints on pub fields
 //may need getters https://github.com/rustwasm/wasm-bindgen/issues/439
-//for the time-being I may just return an array [geo_3d: Float32Array, triangles: Uint16Array].
 #[wasm_bindgen]
 pub struct MarshallGeometry {
     geo_3d: js_sys::Float32Array,
-    _triangles: js_sys::Uint32Array
+    _triangles: js_sys::Uint32Array,
+    // could compute normals and add them here as well
+    // (may consider doing that in JS version as well)
+    // pub computeTime: f64
 }
 #[wasm_bindgen]
 impl MarshallGeometry {
@@ -85,36 +86,26 @@ impl MarshallGeometry {
     }
 }
 
-//#[wasm_bindgen]
-pub type MarshallGeometryTuple = js_sys::Float32Array;//, js_sys::Uint32Array);
 unsafe fn marshall_geometry_to_js(geo_3d: Vec<f32>, _triangles: Vec<usize>) -> MarshallGeometry {
     let geo_js = js_sys::Float32Array::view(&geo_3d);
     //remember: u16 is not enough, tiles may have >65536 vertices
-    // let tri_u32: [u32; triangles.len()];// = for v in triangles.into_iter() {v as u32}
-    // let tri_js = js_sys::Uint32Array::view(&triangles);
+    //maybe I could do something quicker here, meh.
     let mut tri_vec: Vec<u32> = vec!();
     for t in _triangles {
         tri_vec.push(t as u32);
     }
     let tri_js = js_sys::Uint32Array::view(&tri_vec);
 
-    //What is a JsValue for?
-    //Representing objects owned by JS.
-    //Is that what we should be outputting?
-    //Perhaps makes sense so we can hand off ownership and allow GC etc as appropriate.
-    //JsValue::from(geo_js)
     MarshallGeometry{ geo_3d: geo_js, _triangles: tri_js }
-    // JsValue::from(geo_js)
 }
 
 
 struct Contour {
     shape: shapefile::Shape,
-    height: f64
+    height: f64 //could make this f32 sooner rather than later
 }
 
 fn shp_main<R: io::Read + io::Seek>(reader: io::BufReader<R>) -> Result<(Vec<f32>, Vec<usize>), shapefile::Error> {
-    //"G:/GIS/OS Terr50/data/su/su67_OST50CONT_20190530.zip"
     let mut contours: Vec<Contour> = Vec::new();
 
     let mut zip_a = zip::ZipArchive::new(reader)
@@ -125,9 +116,9 @@ fn shp_main<R: io::Read + io::Seek>(reader: io::BufReader<R>) -> Result<(Vec<f32
         let shp_p = format!("{}.shp", t);
         let dbf_p = format!("{}.dbf", t);
         
-        let shp = zip_util::extract_match_to_memory(&mut zip_a, &shp_p)
+        let shp = utils::extract_match_to_memory(&mut zip_a, &shp_p)
             .expect("failed to extract shp");
-        let dbf = zip_util::extract_match_to_memory(&mut zip_a, &dbf_p)
+        let dbf = utils::extract_match_to_memory(&mut zip_a, &dbf_p)
             .expect("failed to extract dbf");
         
         let mut reader = shapefile::Reader::new(shp)?;
@@ -159,25 +150,18 @@ fn shp_main<R: io::Read + io::Seek>(reader: io::BufReader<R>) -> Result<(Vec<f32
 }
 
 fn get_points(contour: &Contour, points: &mut Vec<delaunator::Point>, geo_3d: &mut Vec<f32>) {
-    //let geometry = geo_types::Geometry::<f64>::try_from(shape);
     let height: f64 = contour.height;
     
     match &contour.shape {
         shapefile::Shape::Point(p) => {
             points.push(delaunator::Point{ x: p.x, y: p.y });
             geo_3d.append(&mut vec![p.x as f32, p.y as f32, height as f32]);
-            // geo_3d.push(p.x);
-            // geo_3d.push(p.y);
-            // geo_3d.push(height);
         },
         shapefile::Shape::Polyline(line) => {
             for part in line.parts() {
                 for p in part {
                     points.push(delaunator::Point{ x: p.x, y: p.y });
                     geo_3d.append(&mut vec![p.x as f32, p.y as f32, height as f32]);
-                    // geo_3d.push(p.x);
-                    // geo_3d.push(p.y);
-                    // geo_3d.push(height);
                 }
             }
         }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,29 @@
+use std::io;
+use std::io::*;
+use zip::*;
+use zip::result::{ZipResult, ZipError};
+
+/// find a file ending with the given character string in the archive, 
+/// copy into memory (no longer requiring ownership of archive) and return a cursor to read that memory.
+pub fn extract_match_to_memory<R: Read + io::Seek>(archive: &mut ZipArchive<R>, ending: &str) -> ZipResult<io::Cursor<Vec<u8>>> {
+    //XXXXX::: NO!!!! order of file_names() does not correspond to by_index().
+    // let file_number = archive.file_names().position(|f| f.ends_with(ending)});
+    
+    for file_number in 0..archive.len() {
+        if let Ok(mut file) = archive.by_index(file_number) {
+            if file.name().ends_with(ending) {
+                let mut buffer: Vec<u8> = vec![];
+                let _bytes_read = file.read_to_end(&mut buffer)?;
+                //let arr: &[u8] = &buffer; //works because Vec<T> implements AsRef<[T]>
+                return Ok(io::Cursor::new(buffer));
+            }
+        }
+    }
+    Err(ZipError::Io(Error::new(ErrorKind::NotFound, 
+        "No index found for file name with specified ending.")))
+}
+
+
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
